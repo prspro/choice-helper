84b8b93a6b4841f6e6f47acc5e43c58fbfba4383
prospero <prospero1135@gmail.com>
2023-05-06 22:06:07 +0300

    fonts added

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 // import { Provider } from 'react-redux';
 import App from './App';
+import "normalize.css";
+import "./index.sass";
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
