be782265418436e5454b28fec80a57a88be18a89
prospero <prospero1135@gmail.com>
2023-05-07 18:26:50 +0300

    primary styles added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import ChoiceList from "./components/ChoiceList";
+import Container from "./components/Container";
 
 function App() {
   return (
-    <div>
-      <ChoiceList />
-    </div>
+    <>
+      <header>
+        <Container>Header</Container>
+      </header>
+      <main>
+        <Container>
+          <ChoiceList />
+        </Container>
+      </main>
+      <footer>
+        <Container>footer</Container>
+      </footer>
+    </>
   );
 }
 
