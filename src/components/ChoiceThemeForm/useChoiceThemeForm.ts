5e4af61908ca7df6afcdccf2bb73ded233b3e781
prospero <prospero1135@gmail.com>
2023-05-11 01:47:36 +0300

    style fix and breackpoints added

--- src/components/ChoiceThemeForm/useChoiceThemeForm.ts
+++ src/components/ChoiceThemeForm/useChoiceThemeForm.ts
@@ -1,15 +1,31 @@
 import { IThemeFormValues } from "../../types/types";
-import { useAppDispatch } from "../../store/hooks";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { addChoiceTheme } from "./../../store/slice/appSlice";
 import slugify from "slugify";
 import { nanoid } from "nanoid";
+import * as Yup from "yup";
 
 interface IUseChoiceThemeForm {
   submitHandler: (arg: IThemeFormValues) => void;
 }
 
 const useChoiceThemeForm = (): IUseChoiceThemeForm => {
+
+  // const signupSchema = Yup.object().shape({
+  //   firstName: Yup.string()
+  //     // .min(2, 'Too Short!')
+  //     .max(50, 'Too Long!')
+  //     .required('Required'),
+  //   lastName: Yup.string()
+  //     // .min(2, 'Too Short!')
+  //     .max(50, 'Too Long!')
+  //     .required('Required'),
+  //   email: Yup.string().email('Invalid email').required('Required'),
+  // });
+
   const dispatch = useAppDispatch();
+  const nameList = useAppSelector(state => state.list).map(entry => entry.name);
+  const slugList = useAppSelector(state => state.list).map(entry => entry.slug);
 
   const submitHandler = (value: IThemeFormValues) => {
     dispatch(
