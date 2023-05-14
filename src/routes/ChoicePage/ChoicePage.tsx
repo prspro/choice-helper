06e3b9e9599a62cfc70dc9cf439b41a800123f74
prospero <prospero1135@gmail.com>
2023-05-15 02:38:01 +0300

    theme name edit logic fix

--- src/routes/ChoicePage/ChoicePage.tsx
+++ src/routes/ChoicePage/ChoicePage.tsx
@@ -1,22 +1,63 @@
 import useChoicePage from "./useChoicePage";
 import ChoiceList from "../../components/ChoiceList";
-import EditableField from "../../components/EditableField";
+// import EditableField from "../../components/EditableField";
+import { Formik, Form, Field } from "formik";
 
 type Props = {};
 
 const ChoicePage = (props: Props) => {
-  const { choiceThemeData, isEditing, toggleIsEditing, handleEditThemeName } =
-    useChoicePage();
+  const {
+    choiceThemeData,
+    isThemeEditing,
+    toggleIsThemeEditing,
+    handleEditThemeName,
+    isNameEditing,
+    toggleIsNameEditing,
+    signupSchema
+  } = useChoicePage();
 
   return (
     <>
-      <h2>
+      {!isThemeEditing ? (
+        <h1>{choiceThemeData.name}</h1>
+      ) : (
+        <>
+          {!isNameEditing ? (
+            <>
+              <h1>{choiceThemeData.name}</h1>
+              <button
+                onClick={toggleIsNameEditing}
+              >
+                Edit
+              </button>
+            </>
+          ) : (
+            <Formik
+              initialValues={{ name: choiceThemeData.name }}
+              validationSchema={signupSchema}
+              onSubmit={(values, helpers) => {
+                handleEditThemeName(values.name);
+                helpers.resetForm();
+              }}
+            >
+              {({ values, errors, touched }) => (
+                <Form>
+                  <Field id="name" name="name" />
+                  {errors.name && touched.name ? <div>{errors.name}</div> : null}
+                  <button type="submit">Submit</button>
+                </Form>
+              )}
+            </Formik>
+          )}
+        </>
+      )}
+      {/* <h2>
         <EditableField isEditable={isEditing} handleEdit={handleEditThemeName}>
           {choiceThemeData.name}
         </EditableField>
-      </h2>
-      <button onClick={toggleIsEditing}>Edit</button>
-      <ChoiceList isEditable={isEditing} themeData={choiceThemeData} />
+      </h2> */}
+      <button onClick={toggleIsThemeEditing}>Edit theme</button>
+      <ChoiceList isEditable={isThemeEditing} themeData={choiceThemeData} />
     </>
   );
 };
--- src/routes/ChoicePage/useChoicePage.ts
+++ src/routes/ChoicePage/useChoicePage.ts
@@ -5,13 +5,27 @@ import { useAppSelector, useAppDispatch } from "../../store/hooks";
 import { IChoiceThemeData } from "../../types/types";
 import { editChoiceTheme } from "../../store/slice/appSlice";
 import slugify from "slugify";
+import * as Yup from "yup";
+import { nanoid } from "nanoid";
 
 // interface IUseChoicePageProps {}
 interface IUseChoicePage {
   choiceThemeData: IChoiceThemeData;
-  toggleIsEditing: () => void;
-  isEditing: boolean;
+  toggleIsThemeEditing: () => void;
+  isThemeEditing: boolean;
   handleEditThemeName: (newName: string) => void;
+  isNameEditing: boolean;
+  toggleIsNameEditing: () => void;
+  signupSchema: Yup.ObjectSchema<
+    {
+      name: string;
+    },
+    Yup.AnyObject,
+    {
+      name: undefined;
+    },
+    ""
+  >;
 }
 
 const useChoicePage = (): IUseChoicePage => {
@@ -19,24 +33,57 @@ const useChoicePage = (): IUseChoicePage => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
 
-  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [isThemeEditing, setisThemeEditing] = useState<boolean>(false);
+  const [isNameEditing, setIsNameEditing] = useState<boolean>(false);
 
-  const toggleIsEditing = () => {
-    setIsEditing((value) => !value);
+  const toggleIsThemeEditing = () => {
+    setisThemeEditing((value) => !value);
   };
+
+  const toggleIsNameEditing = () => {
+    setIsNameEditing((value) => !value);
+  };
+
+  const nameList = useAppSelector((state) => state.list).map(
+    (entry) => entry.name
+  );
+  const slugList = useAppSelector((state) => state.list).map(
+    (entry) => entry.slug
+  );
+  const signupSchema = Yup.object().shape({
+    name: Yup.string()
+      // .min(2, 'Too Short!')
+      .max(50, "Too Long!")
+      .required("Required")
+      .notOneOf(nameList, "name is already used"),
+  });
+
+  // const handleEditThemeName = (newName: string) => {
+  //   //! need to be reworked bcs of possible name duplicating (Formik with Yup?)
+  //   const newSlug = slugify(newName)
+  //   dispatch(
+  //     editChoiceTheme({
+  //       id: choiceThemeData.id,
+  //       slug: newSlug,
+  //       name: newName,
+  //     })
+  //   );
+  //   navigate(`/theme/${newSlug}`);
+  // };
 
   const handleEditThemeName = (newName: string) => {
     //! need to be reworked bcs of possible name duplicating (Formik with Yup?)
-    const newSlug = slugify(newName)
+    const newSlug = slugify(newName.toLowerCase());
+    toggleIsNameEditing();
     dispatch(
       editChoiceTheme({
         id: choiceThemeData.id,
-        slug: newSlug,
+        slug: slugList.includes(newSlug) ? `${newSlug}-${nanoid(5)}` : newSlug,
         name: newName,
       })
     );
     navigate(`/theme/${newSlug}`);
-  }
+  };
 
   const themeData = useAppSelector((state) => state.list)?.find(
     (entry) => entry.slug === slug
@@ -57,9 +104,12 @@ const useChoicePage = (): IUseChoicePage => {
 
   return {
     choiceThemeData,
-    toggleIsEditing,
-    isEditing,
+    toggleIsThemeEditing,
+    isThemeEditing,
     handleEditThemeName,
+    isNameEditing,
+    toggleIsNameEditing,
+    signupSchema,
   };
 };
 
