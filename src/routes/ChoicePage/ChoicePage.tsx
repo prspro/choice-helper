016ef53070f5fd80748841cd2515c342b4e7f070
prospero <prospero1135@gmail.com>
2023-05-17 23:25:58 +0300

    some design reworked

--- src/routes/ChoicePage/ChoicePage.tsx
+++ src/routes/ChoicePage/ChoicePage.tsx
@@ -1,69 +1,20 @@
+import { FC } from "react";
 import useChoicePage from "./useChoicePage";
 import ChoiceList from "../../components/ChoiceList";
-// import EditableField from "../../components/EditableField";
-import { Formik, Form, Field } from "formik";
 import RandomPicker from "../../components/RandomPicker";
 import { GridItem, GridList } from "../../components/GridList";
 
-type Props = {};
+interface IChoicePageProps {}
 
-const ChoicePage = (props: Props) => {
-  const {
-    choiceThemeData,
-    isThemeEditing,
-    toggleIsThemeEditing,
-    handleEditThemeName,
-    isNameEditing,
-    toggleIsNameEditing,
-    signupSchema,
-  } = useChoicePage();
+const ChoicePage: FC<IChoicePageProps> = () => {
+  const { choiceThemeData } = useChoicePage();
 
   return (
     <>
-      <div>
-        {!isThemeEditing ? (
-          <h1>{choiceThemeData.name}</h1>
-        ) : (
-          <>
-            {!isNameEditing ? (
-              <>
-                <h1>{choiceThemeData.name}</h1>
-                <button onClick={toggleIsNameEditing}>Edit</button>
-              </>
-            ) : (
-              <Formik
-                initialValues={{ name: choiceThemeData.name }}
-                validationSchema={signupSchema}
-                onSubmit={(values, helpers) => {
-                  handleEditThemeName(values.name);
-                  helpers.resetForm();
-                }}
-              >
-                {({ values, errors, touched }) => (
-                  <Form>
-                    <Field id="name" name="name" />
-                    {errors.name && touched.name ? (
-                      <div>{errors.name}</div>
-                    ) : null}
-                    <button type="submit">Submit</button>
-                  </Form>
-                )}
-              </Formik>
-            )}
-          </>
-        )}
-        {/* <h2>
-        <EditableField isEditable={isEditing} handleEdit={handleEditThemeName}>
-          {choiceThemeData.name}
-        </EditableField>
-      </h2> */}
-        <button onClick={toggleIsThemeEditing}>
-          Edit theme
-        </button>
-      </div>
+      <h2>{choiceThemeData.name}</h2>
       <GridList>
         <GridItem>
-          <ChoiceList isEditable={isThemeEditing} themeData={choiceThemeData} />
+          <ChoiceList isEditable={false} themeData={choiceThemeData} />
         </GridItem>
         <GridItem>
           <RandomPicker choiceList={choiceThemeData.list} />
